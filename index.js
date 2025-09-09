5bdec2006a227870f925125e5b96b6a492419b07
RishabhRawat121 <rishabhrawat150leetcode@gmail.com>
2025-09-09 18:38:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5174", 
-    credentials: true, 
+    origin: "*", // allow all origins
   })
 );
 
